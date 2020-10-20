6c5f4d0018fe8ed38dd0457d1af86a8c23624b76
pallav tripathi <pallavtripathi93@gmail.com>
2020-10-20 20:47:58 +0530

    fourth commit

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -18,6 +18,7 @@ export default (req, store, context) => {
     <!DOCTYPE html>
     <html>
         <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link
                 rel="stylesheet"
                 type="text/css"
